dde013bd9a09fa395d485cc0e70e3a22c4ae5929
aya bassel <ayabassell@gmail.com>
2023-06-27 14:29:06 +0300

    'serializers_adminSettings'

--- Django/e_commerce/products/admin.py
+++ Django/e_commerce/products/admin.py
@@ -1,2 +1,15 @@
+from django.contrib import admin
+from .models import ProductCategory, ProductItem, Promotion
+from django.db import models
 
 
+admin.site.register(ProductCategory)
+admin.site.register(Promotion)
+
+class ProductAdmin(admin.ModelAdmin):
+    # make the discount_price field optional in the admin dashboard
+    formfield_overrides = {
+        models.FloatField: {'required': False},
+    }
+
+admin.site.register(ProductItem, ProductAdmin)
--- Django/e_commerce/products/serializers.py
+++ Django/e_commerce/products/serializers.py
@@ -1,2 +1,18 @@
+from rest_framework import serializers
+from products.models import ProductCategory, ProductItem, Promotion
 
+class ProductCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductCategory
+        fields = ['name', 'slug', 'category_items']
+        
+class ProductItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductItem
+        fields = '_all_'
+        
+class PromotionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Promotion
+        fields = ['start_date', 'slug', 'discount_rate', 'end_date', 'promotion_items']
                         
